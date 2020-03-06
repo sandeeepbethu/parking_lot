28bca9c00442dcf52f52c61743291d5306a36667
Sandeep.Bethu <sandeep.bethu@target.com>
2020-03-06 10:19:15 +0530

    Added core svc, sample methid for loading input

--- src/main/java/com/gojek/assessment/ApplicationStart.java
+++ src/main/java/com/gojek/assessment/ApplicationStart.java
@@ -1,11 +1,20 @@
 package com.gojek.assessment;
 
+import com.gojek.assessment.service.CoreService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApplicationStart {
+
+    private static CoreService coreService = null;
+
+    public ApplicationStart(CoreService coreService) {
+        this.coreService = coreService;
+    }
+
     public static void main(String[] arguments) {
         SpringApplication.run(ApplicationStart.class);
+        coreService.loadInput();
     }
 }
---
+++ src/main/java/com/gojek/assessment/service/CoreService.java
@@ -0,0 +1,10 @@
+package com.gojek.assessment.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CoreService {
+    public void loadInput() {
+        System.out.println("hello");
+    }
+}
