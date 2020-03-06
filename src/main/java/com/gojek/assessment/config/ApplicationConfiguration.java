0db05ef57a2b3a24c2ca48dcf8eb510d976869bd
Sandeep.Bethu <sandeep.bethu@target.com>
2020-03-06 09:52:58 +0530

    Load of config file during launch added

---
+++ src/main/java/com/gojek/assessment/config/ApplicationConfiguration.java
@@ -0,0 +1,15 @@
+package com.gojek.assessment.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties
+@ConfigurationProperties("local")
+@Getter
+@Setter
+public class ApplicationConfiguration {
+}
